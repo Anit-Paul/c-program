6df6c584ff3fcbf488879b4ae7b46e2b1d8e8ec2
Anit Paul <anit42655@gmail.com>
2025-12-20 11:50:53 +0530

    fix bug on bugFix3.c

--- bugFix3.c
+++ bugFix3.c
@@ -2,7 +2,14 @@
 #include <stdlib.h>
 
 int* createArray() {
-    int arr[3] = {10, 20, 30};
+    int *arr=(int*)malloc(3*sizeof(int));
+    if(!arr){
+        printf("memory allocation fails for arr\n");
+        exit(0);
+    }
+    arr[0]=10;
+    arr[1]=20;
+    arr[2]=30;
     return arr;
 }
 
@@ -12,10 +19,13 @@ void modify(int *p) {
 
 int main() {
     int *ptr = createArray();
+    if(!ptr){
+        printf("memory allocation fails\n");
+        exit(0);
+    }
     modify(ptr);
 
     printf("%d\n", ptr[0]);
     free(ptr);
-
     return 0;
 }
