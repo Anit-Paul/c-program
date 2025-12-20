a063fe9e776d10b2bee733fa3dee6e227f99e8b5
Anit Paul <anit42655@gmail.com>
2025-12-20 11:35:29 +0530

    bug fixed

--- bugFix2.c
+++ bugFix2.c
@@ -11,22 +11,36 @@ void init(Buffer *buf) {
     buf->size = 0;
     buf->capacity = 2;
     buf->data = malloc(buf->capacity * sizeof(int));
+    if(!buf->data){
+        printf("memory allocation fails\n");
+        exit(0);
+    }
 }
 
 void push(Buffer *buf, int value) {
     if (buf->size == buf->capacity) {
         buf->capacity *= 2;
-        buf->data = realloc(buf->data, buf->capacity * sizeof(int));
+        int *temp = realloc(buf->data, buf->capacity * sizeof(int));
+        if(!temp){
+            printf("memory allocation fails\n");
+            exit(0);
+        }
+        buf->data=temp;
     }
     buf->data[buf->size++] = value;
 }
 
 void destroy(Buffer *buf) {
+    free(buf->data);
     free(buf);
 }
 
 int main() {
     Buffer *buf = malloc(sizeof(Buffer));
+    if(!buf){
+        printf("memory allocation fails\n");
+        exit(0);
+    }
     init(buf);
 
     push(buf, 10);
